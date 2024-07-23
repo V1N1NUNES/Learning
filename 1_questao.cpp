264a1e560e2b9f085b8916f98d2bd2e6fa1700b0
Vinicius Nunes <165539702+V1N1NUNES@users.noreply.github.com>
2024-07-23 15:50:16 -0300

    ai sim meu parceirinho

--- 1_questao.cpp
+++ 1_questao.cpp
@@ -2,11 +2,15 @@
 #include <unistd.h>
 
 int main (){
-    printf (" sera que eu esqueci como que coda em C???\n");
+    printf (" sera que eu esqueci como que codar em C???\n");
 
     sleep(3);
+
+    printf("Ou sera que......\n");
+
+    sleep(3.5);
     
-    printf("acho que to me alembrando.\n");
+    printf("acho que to me alembrando\nmeu cumpassa.\n");
 
     return 0;
 }
