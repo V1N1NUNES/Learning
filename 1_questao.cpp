31856133f8032fc1fb755b2e468b6e6f94538909
Vinicius Nunes <165539702+V1N1NUNES@users.noreply.github.com>
2024-07-23 15:45:02 -0300

    agora sim

--- 1_questao.cpp
+++ 1_questao.cpp
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main (){
     printf (" sera que eu esqueci como que coda em C???\n");
+
+    sleep(3);
+    
+    printf("acho que to me alembrando.\n");
 
     return 0;
 }
